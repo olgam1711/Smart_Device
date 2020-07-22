fcc2b4bae78a5a3e2dd92eb802ca8fdf752665fc
Ольга <onmorozova24@gmail.com>
2020-07-22 21:47:51 +0300

    accordion footer mobile

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,16 @@
 'use sctrict'
+
+var contentsButton = document.querySelector('.contents__button')
+var contactsButton = document.querySelector('.contacts__button')
+var contents = document.querySelector('.contents')
+var contacts = document.querySelector('.contacts')
+
+contentsButton.addEventListener('click', function (evt) {
+  contents.classList.toggle('contents--closed')
+  console.log(123)
+})
+
+contactsButton.addEventListener('click', function (evt) {
+  contacts.classList.toggle('contacts--closed')
+  console.log(123)
+})
