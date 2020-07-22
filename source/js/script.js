f88f1d0f6e6f06a297b06901e2636373c7f75a97
Ольга <onmorozova24@gmail.com>
2020-07-22 23:51:31 +0300

    header modal overlay

--- source/js/script.js
+++ source/js/script.js
@@ -14,24 +14,31 @@ contactsButton.addEventListener('click', function (evt) {
 })
 
 var callback = document.querySelector('.callback__element--call')
-var modalCallback = document.querySelector('.modal-callback')
+var overlay = document.querySelector('.overlay')
 var modalClose = document.querySelector('.modal-callback__cross-button')
 
 callback.addEventListener('click', function (evt) {
   evt.preventDefault()
-  modalCallback.classList.add('modal-callback--open')
+  overlay.classList.add('overlay--open')
 })
 
 modalClose.addEventListener('click', function (evt) {
   evt.preventDefault()
-  modalCallback.classList.remove('modal-callback--open')
+  overlay.classList.remove('overlay--open')
 })
 
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault()
-    if (modalCallback.classList.contains('modal-callback--open')) {
-      modalCallback.classList.remove('modal-callback--open')
+    if (overlay.classList.contains('overlay--open')) {
+      overlay.classList.remove('overlay--open')
     }
+  }
+})
+
+overlay.addEventListener('click', function (evt) {
+  if (evt.target === overlay) {
+    evt.preventDefault()
+    overlay.classList.remove('overlay--open')
   }
 })
