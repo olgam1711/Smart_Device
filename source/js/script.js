8e3d462090e7b9216cce3c0c32e85a0d9f8d59b5
Ольга <onmorozova24@gmail.com>
2020-08-07 14:58:41 +0300

    deleted extra elements

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,5 @@
 'use sctrict'
 
-var contentsButton = document.querySelector('.contents__button')
-var contactsButton = document.querySelector('.contacts__button')
-var contents = document.querySelector('.contents')
-var contacts = document.querySelector('.contacts')
-
-contentsButton.addEventListener('click', function (evt) {
-  contents.classList.toggle('contents--closed')
-})
-
-contactsButton.addEventListener('click', function (evt) {
-  contacts.classList.toggle('contacts--closed')
-})
-
 var callback = document.querySelector('.callback__element--call')
 var overlay = document.querySelector('.overlay')
 var modalClose = document.querySelector('.modal-callback__cross-button')
